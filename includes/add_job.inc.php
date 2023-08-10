7f7c0d6606e069a510a274800882605949c4d318
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-08-10 18:00:38 +0300

    Update add_job.inc.php
    - change from taking json input to multi-part-form via $_POS
    - check whether form content length exceed the max set in php.ini

--- includes/add_job.inc.php
+++ includes/add_job.inc.php
@@ -4,23 +4,38 @@ include_once('db.inc.php');
 include_once('functions.inc.php');
 include_once('../utils/respond.php');
 
-$content = trim(file_get_contents("php://input"));
-
-$decoded = json_decode($content, true);
-
-  //If json_decode failed, the JSON is invalid.
-if( is_array($decoded)) {
-    try {
-        $job = addJob($conn, $decoded);
-        if (!$job) {
-            throw new Exception("no new job returned", 500);
-        }
-        
-        echo json_encode($job);
-        exit();
-    } catch (Exception $e) {
-        respondWith($e->getCode(), $e->getMessage());
+function convertToBytes($value) {
+    $limit = trim(strtolower($value), 'mkg');
+    $last = strtolower($value[strlen($value)-1]);
+  
+    switch($last) {
+      case 'g':
+        $limit *= 1000000000;
+        break;
+      case 'm':
+        $limit *= 1000000;
+        break;
+      case 'k':
+        $limit *= 1000;
+        break;
     }
-} else {
-    respondWith(500, 'Invalid json');
+    return $limit;
+  }
+
+if (isset($_SERVER['CONTENT_LENGTH']) 
+    && (int) $_SERVER['CONTENT_LENGTH'] > convertToBytes(ini_get('post_max_size'))) 
+{
+    respondWith(400, 'File too large');
+}
+
+try {
+    $job = addJob($conn, $_POST);
+    if (!$job) {
+        throw new Exception("no new job returned", 500);
+    }
+    
+    echo json_encode($job);
+    exit();
+} catch (Exception $e) {
+    respondWith($e->getCode(), $e->getMessage());
 }
