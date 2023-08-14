e94a50915a4254f862880c4dd9efe7e6501b069b
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-08-14 15:15:14 +0300

    move convertToBytes to utils/convert.php

--- includes/add_job.inc.php
+++ includes/add_job.inc.php
@@ -3,24 +3,7 @@
 include_once('db.inc.php');
 include_once('functions.inc.php');
 include_once('../utils/respond.php');
-
-function convertToBytes($value) {
-    $limit = trim(strtolower($value), 'mkg');
-    $last = strtolower($value[strlen($value)-1]);
-  
-    switch($last) {
-      case 'g':
-        $limit *= 1000000000;
-        break;
-      case 'm':
-        $limit *= 1000000;
-        break;
-      case 'k':
-        $limit *= 1000;
-        break;
-    }
-    return $limit;
-  }
+include_once('../utils/convert.php');
 
 if (isset($_SERVER['CONTENT_LENGTH']) 
     && (int) $_SERVER['CONTENT_LENGTH'] > convertToBytes(ini_get('post_max_size'))) 
