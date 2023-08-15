6bf2022d24e33f27125cf9cfbf572a2b3b625aeb
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-08-15 10:32:39 +0300

    Update functions.inc.php
    - add check for if client by supplied name already exists and handle the error appropriately

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -140,12 +140,17 @@ function addClient(mysqli $conn, array $client) {
     if (!isAuthorised(2)) {
         throw new Exception("You are not authorised for this operation!", 401);
     }
+    $clientName = $client['name'];
+    $clientExists = queryRow($conn, "client exists", "SELECT * FROM jc_clients WHERE `name` = ?;", 's', $clientName);
+    if ($clientExists) {
+        throw new Exception("client with name $clientName already exists", 400);
+    }
     $sql = 'INSERT INTO jc_clients (`name`, email, `phone`, `location`, created_at) VALUES (?, ?, ?, ?, null);';
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
         throw new Exception("Error preparing sql statement: ". mysqli_stmt_error($stmt), 500);
     }
-    $boundOk = mysqli_stmt_bind_param($stmt, 'ssss', $client['name'], $client['email'], $client['phone'], $client['location']);
+    $boundOk = mysqli_stmt_bind_param($stmt, 'ssss', $clientName, $client['email'], $client['phone'], $client['location']);
     if(!$boundOk) {
         throw new Exception("Invalid params: ".mysqli_stmt_error($stmt), 400);
     }
