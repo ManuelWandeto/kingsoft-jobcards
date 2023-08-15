2bb6f7ec4b0fed62601a4b449f2068c472b3d5d6
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-08-15 21:45:18 +0300

    Update Kingsoft Jobcards.v1.down.sql

--- db/Kingsoft Jobcards.v1.down.sql
+++ db/Kingsoft Jobcards.v1.down.sql
@@ -2,3 +2,5 @@ DROP TABLE `jc_jobcards` CASCADE;
 DROP TABLE `jc_attachments` CASCADE;
 DROP TABLE `jc_users` CASCADE;
 DROP TABLE `jc_clients` CASCADE;
+DROP TABLE `jc_jobcard_tags` CASCADE;
+DROP TABLE `jc_tags` CASCADE;
