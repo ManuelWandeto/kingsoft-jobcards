21a0f5133d61bff0a1941a2e8b359c0f33738907
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-08-22 10:19:10 +0300

    Update users_section.php

--- users_section.php
+++ users_section.php
@@ -1,6 +1,6 @@
 <div style="width: 100%;" id="user-section">
     <div class="row row-cols-xl-2 px-3" x-show="showUserSection" x-cloak x-transition>
-        <?php include_once('clients.php')?>
-        <?php include_once('users.php')?>
+        <?php require_once('clients.php')?>
+        <?php require_once('users.php')?>
     </div>
 </div>
