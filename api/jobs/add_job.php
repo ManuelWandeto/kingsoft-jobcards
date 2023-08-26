48881a282a70d3dc7ef0b9bf9384f8fa502de193
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-08-26 22:39:05 +0300

    Update add_job.php
    send report when job is added (commented out for now)

--- api/jobs/add_job.php
+++ api/jobs/add_job.php
@@ -4,6 +4,7 @@ require_once('../../db/db.inc.php');
 require_once('../../db/queries/jobs.inc.php');
 require_once('../../utils/respond.php');
 require_once('../../utils/convert.php');
+require_once('../../messaging/report.php');
 
 if (isset($_SERVER['CONTENT_LENGTH']) 
     && (int) $_SERVER['CONTENT_LENGTH'] > convertToBytes(ini_get('post_max_size'))) 
@@ -16,6 +17,13 @@ try {
     if (!$job) {
         throw new Exception("no new job returned", 500);
     }
+    // sendWhatsappMessage(
+    //     "New Jobcard added under {$job['project']}\n
+    //     Description: {$job['description']}
+    //     Status: {$job['status']}
+    //     Startdate: {$job['start_date']}
+    //     Enddate: {$job['end_date']}"
+    // );
     
     echo json_encode($job);
     exit();
