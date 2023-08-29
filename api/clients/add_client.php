8517b312d0bc8152efee39a2d20aef20ed21f372
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-08-29 18:45:38 +0300

    Update add_client.php
    - change from accepting json content type to multipart/formdata
    -

--- api/clients/add_client.php
+++ api/clients/add_client.php
@@ -4,25 +4,16 @@ require_once('../../db/db.inc.php');
 require_once('../../db/queries/clients.inc.php');
 require_once('../../utils/respond.php');
 
-$content = trim(file_get_contents("php://input"));
-
-$decoded = json_decode($content, true);
-
-  //If json_decode failed, the JSON is invalid.
-if( is_array($decoded)) {
-    try {
-        $client = addClient($conn, $decoded);
-        if (!$client) {
-            throw new Exception("no new client returned", 500);
-        }
-        $clientJson = json_encode($client);
-        if(!$clientJson) {
-            throw new Exception("error encoding new client to json", 500);
-        }
-        echo $clientJson;
-    } catch (Exception $e) {
-        respondWith($e->getCode(), $e->getMessage());
+try {
+    $client = addClient($conn, $_POST);
+    if (!$client) {
+        throw new Exception("no new client returned", 500);
     }
-} else {
-    respondWith(500, 'invalid json');
+    $clientJson = json_encode($client);
+    if(!$clientJson) {
+        throw new Exception("error encoding new client to json", 500);
+    }
+    echo $clientJson;
+} catch (Exception $e) {
+    respondWith($e->getCode(), $e->getMessage());
 }
