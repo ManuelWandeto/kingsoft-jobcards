eebe1d2797839860dc204bc411eb59cad0ca6380
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-09-06 10:49:00 +0300

    Update get_jobs.php
    use PDO connection for getJobs

--- api/jobs/get_jobs.php
+++ api/jobs/get_jobs.php
@@ -6,8 +6,9 @@ require_once('../../utils/respond.php');
 // if(!isset($_SESSION["user_id"])) {
 //     session_start();
 // }
+
 try {
-    $jobs = getJobs($conn);
+    $jobs = getJobs($pdo_conn, $_GET);
     echo json_encode($jobs);
     exit();
 } catch (Exception $e) {
