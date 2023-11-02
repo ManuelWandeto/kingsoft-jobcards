062dbca62fb3d24ff7f5fcc3fba937fbe734df4e
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-11-02 10:10:39 +0300

    Create jobs_per_day.php

---
+++ api/reports/jobs_per_day.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../../db/db.inc.php');
+require_once('../../db/queries/reports.inc.php');
+require_once('../../utils/respond.php');
+require_once('../../utils/logger.php');
+
+try {
+    $report = getJobsPerDay($pdo_conn, $_GET, $dbLogger);
+    echo json_encode($report);
+    exit();
+} catch (Exception $e) {
+    respondWith($e->getCode(), $e->getMessage());
+}
